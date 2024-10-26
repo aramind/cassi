eed4f6bab17d48ccc1ebd7832dfd8fb92c5f3032
aramind <88814622+aramind@users.noreply.github.com>
2024-10-26 22:54:58 +0800

    feat: adding images for regiterSignin pages

---
+++ client/src/pages/login-register/LogInRegisterMain.jsx
@@ -0,0 +1,8 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const LogInRegisterMain = () => {
+  return <Typography>LogInRegisterMain</Typography>;
+};
+
+export default LogInRegisterMain;
