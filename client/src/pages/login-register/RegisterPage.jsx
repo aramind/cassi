c2e0fa0cb9234e89ec774b5f8798c01ea0eb1c96
aramind <88814622+aramind@users.noreply.github.com>
2024-10-28 00:00:00 +0800

    feat: adding email text field

--- client/src/pages/login-register/RegisterPage.jsx
+++ client/src/pages/login-register/RegisterPage.jsx
@@ -6,6 +6,7 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import registerSchema from "../../schemas/registerSchema";
 import FormWrapper from "../../wrappers/FormWrapper";
+import ControlledTextField from "../../components/controlled/ControlledTextField";
 
 const RegisterPage = () => {
   const {
@@ -27,27 +28,37 @@ const RegisterPage = () => {
     alert("submitting...");
     console.log("Submitting ", data);
   };
+
   return (
     <FormWrapper formMethods={formMethods}>
-      <BodyContainer>
-        <Stack width={{ xs: "90%", md: "500px" }} className=" centered ">
-          <HeroImage
-            bgcolor={(theme) => theme.palette.primary.light}
-            width={{ xs: "220px", md: "300px" }}
-            height={{ xs: "220px", md: "300px" }}
-            src={`/assets/images/register.png`}
-          />
-          <Typography variant="mediumHeader">Get Started!</Typography>
-          <br />
-          <Typography variant="smallSubHeader" textAlign="center">
-            Sign up and start managing with ease!
-          </Typography>
-          <br />
-          <Button fullWidth variant="contained">
-            register
-          </Button>
-        </Stack>
-      </BodyContainer>
+      <form noValidate>
+        <BodyContainer>
+          <Stack width={{ xs: "90%", md: "500px" }} className=" centered ">
+            <HeroImage
+              bgcolor={(theme) => theme.palette.primary.light}
+              width={{ xs: "220px", md: "300px" }}
+              height={{ xs: "220px", md: "300px" }}
+              src={`/assets/images/register.png`}
+            />
+            <Typography variant="mediumHeader">Get Started!</Typography>
+            <br />
+            <Typography variant="smallSubHeader" textAlign="center">
+              Sign up and start managing with ease!
+            </Typography>
+            <br />
+            <Stack spacing={1} width={1}>
+              <ControlledTextField name="email" label="Email" />
+            </Stack>
+            <Button
+              fullWidth
+              variant="contained"
+              onClick={handleSubmit(onSubmit)}
+            >
+              register
+            </Button>
+          </Stack>
+        </BodyContainer>
+      </form>
     </FormWrapper>
   );
 };
