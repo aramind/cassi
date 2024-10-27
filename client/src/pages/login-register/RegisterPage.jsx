1b3e6175acb91a20392f7b73d11090071b4b2858
aramind <88814622+aramind@users.noreply.github.com>
2024-10-27 23:36:54 +0800

    feat: wrapping the register comp by form wrapper

--- client/src/pages/login-register/RegisterPage.jsx
+++ client/src/pages/login-register/RegisterPage.jsx
@@ -2,28 +2,53 @@ import React from "react";
 import BodyContainer from "../../containers/BodyContainer";
 import { Button, Stack, Typography } from "@mui/material";
 import HeroImage from "./HeroImage";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import registerSchema from "../../schemas/registerSchema";
+import FormWrapper from "../../wrappers/FormWrapper";
 
 const RegisterPage = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onTouched",
+    resolver: yupResolver(registerSchema),
+  });
+
+  const formMethods = {
+    control,
+    handleSubmit,
+    errors,
+  };
+
+  const onSubmit = (data) => {
+    alert("submitting...");
+    console.log("Submitting ", data);
+  };
   return (
-    <BodyContainer>
-      <Stack width={{ xs: "90%", md: "500px" }} className=" centered ">
-        <HeroImage
-          bgcolor={(theme) => theme.palette.primary.light}
-          width={{ xs: "220px", md: "300px" }}
-          height={{ xs: "220px", md: "300px" }}
-          src={`/assets/images/register.png`}
-        />
-        <Typography variant="mediumHeader">Get Started!</Typography>
-        <br />
-        <Typography variant="smallSubHeader" textAlign="center">
-          Sign up and start managing with ease!
-        </Typography>
-        <br />
-        <Button fullWidth variant="contained">
-          register
-        </Button>
-      </Stack>
-    </BodyContainer>
+    <FormWrapper formMethods={formMethods}>
+      <BodyContainer>
+        <Stack width={{ xs: "90%", md: "500px" }} className=" centered ">
+          <HeroImage
+            bgcolor={(theme) => theme.palette.primary.light}
+            width={{ xs: "220px", md: "300px" }}
+            height={{ xs: "220px", md: "300px" }}
+            src={`/assets/images/register.png`}
+          />
+          <Typography variant="mediumHeader">Get Started!</Typography>
+          <br />
+          <Typography variant="smallSubHeader" textAlign="center">
+            Sign up and start managing with ease!
+          </Typography>
+          <br />
+          <Button fullWidth variant="contained">
+            register
+          </Button>
+        </Stack>
+      </BodyContainer>
+    </FormWrapper>
   );
 };
 
