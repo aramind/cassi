d2efef094c4c9608bd927741da878caa7cc231f3
aramind <88814622+aramind@users.noreply.github.com>
2024-10-27 23:28:30 +0800

    feat: adding register schema

---
+++ client/src/schemas/registerSchema.js
@@ -0,0 +1,16 @@
+import * as y from "yup";
+
+const notEmpty = "This field cannot be empty";
+const registerSchema = y.object().shape({
+  email: y.string().required(`${notEmpty}`).email("Invalid email address"),
+  homeName: y
+    .string()
+    .required(`${notEmpty}`)
+    .min(3, "Must be at least 3 characters long"),
+  password: y
+    .string()
+    .required(`${notEmpty}`)
+    .min(4, "Must be at least 4 characters long"),
+});
+
+export default registerSchema;
