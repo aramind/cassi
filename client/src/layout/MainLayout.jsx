bc45dbc3a6fad10e6533e9a189f80531662e32c1
aramind <88814622+aramind@users.noreply.github.com>
2024-10-28 22:33:24 +0800

    feat: adding main layout

---
+++ client/src/layout/MainLayout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "@mui/icons-material";
+import React from "react";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+};
+
+export default MainLayout;
