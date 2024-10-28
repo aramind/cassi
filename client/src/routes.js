04154301ee28af0a6539133333a0d81ff95e1aac
aramind <88814622+aramind@users.noreply.github.com>
2024-10-28 22:37:59 +0800

    feat: adding initial routes

---
+++ client/src/routes.js
@@ -0,0 +1,30 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import RegisterPage from "./pages/login-register/RegisterPage";
+import LoginPage from "./pages/login-register/LoginPage";
+import Landing from "./pages/login-register/Landing";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/register",
+        element: <RegisterPage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "*",
+        element: <Landing />,
+      },
+    ],
+  },
+]);
+
+const routes = { router };
+
+export default routes;
