c4c657b12dfbe5b54b70b82775380cee303e021d
aramind <88814622+aramind@users.noreply.github.com>
2024-10-29 23:11:14 +0800

    feat: adding dashboard main page

---
+++ client/src/pages/dashboard/DashBoardMain.jsx
@@ -0,0 +1,12 @@
+import { Stack, Typography } from "@mui/material";
+import React from "react";
+
+const DashBoardMain = () => {
+  return (
+    <Stack>
+      <Typography variant="mediumHeader">Dashboard</Typography>
+    </Stack>
+  );
+};
+
+export default DashBoardMain;
