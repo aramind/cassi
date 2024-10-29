8caf45231d130ced9bfa091e4323d5ecd015b594
aramind <88814622+aramind@users.noreply.github.com>
2024-10-29 23:23:23 +0800

    feat: displaying day and date on dashboard

--- client/src/pages/dashboard/DashBoardMain.jsx
+++ client/src/pages/dashboard/DashBoardMain.jsx
@@ -1,10 +1,16 @@
 import { Stack, Typography } from "@mui/material";
 import React from "react";
+import { getCurrentDay, getCurrentDate } from "../../utils/date";
 
 const DashBoardMain = () => {
   return (
     <Stack>
-      <Typography variant="mediumHeader">Dashboard</Typography>
+      <Typography variant="mediumHeader" fontSize="1.8rem">
+        Hi! Happy {getCurrentDay}!
+      </Typography>
+      <Typography variant="smallSubHeader" fontSize="1.2rem">
+        Today is {getCurrentDate}.
+      </Typography>
     </Stack>
   );
 };
