eaf0c38abe8214991dc70a1f375e235794d621e6
aramind <88814622+aramind@users.noreply.github.com>
2024-10-31 15:19:55 +0800

    feat: adding header and subheader for trackers page

--- client/src/pages/trackers/TrackersPage.jsx
+++ client/src/pages/trackers/TrackersPage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import BodyContainer from "../../containers/BodyContainer";
+import PageHeader from "../../components/PageHeader";
+import { Stack } from "@mui/material";
+import Today from "../../components/Today";
 
 const TrackersPage = () => {
-  return <div>TrackersPage</div>;
+  return (
+    <BodyContainer justifyContent="flex-start">
+      <Stack mt={2} alignItems="center">
+        <PageHeader text="trackers " />
+        <Today />
+      </Stack>
+    </BodyContainer>
+  );
 };
 
 export default TrackersPage;
