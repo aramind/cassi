fb192261b6281395ee0176d280095b92c135d4bc
aramind <88814622+aramind@users.noreply.github.com>
2024-10-31 15:07:57 +0800

    feat: extracting into separate component the text to display the date

---
+++ client/src/components/Today.jsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+import React from "react";
+import { getCurrentDate } from "../utils/date";
+
+const Today = () => {
+  return <Typography variant="h6">Today is {getCurrentDate}.📅</Typography>;
+};
+
+export default Today;
