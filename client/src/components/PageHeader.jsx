542c2e6552d555a769ddca34d28404166d2f8f40
aramind <88814622+aramind@users.noreply.github.com>
2024-10-31 15:19:16 +0800

    refactor: capitalizing headers

--- client/src/components/PageHeader.jsx
+++ client/src/components/PageHeader.jsx
@@ -2,7 +2,11 @@ import { Typography } from "@mui/material";
 import React from "react";
 
 const PageHeader = ({ text }) => {
-  return <Typography variant="h5">{text}</Typography>;
+  return (
+    <Typography variant="h5" textTransform="capitalize">
+      {text}
+    </Typography>
+  );
 };
 
 export default PageHeader;
