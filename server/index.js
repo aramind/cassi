82928d76cb71de2bcfe1d02b48fd104e1cf3f965
aramind <88814622+aramind@users.noreply.github.com>
2024-11-20 23:26:46 +0800

    setup: adding server starter setup

---
+++ server/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+// env
+dotenv.config();
+const { PORT = 3001, MONGO_CONNECT: DB } = process.env;
+
+const app = express();
+
+// CORS
+app.use(cookieParser());
+app.use(cors());
+app.use([helmet(), express.json()]);
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+// for the static site
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+// if not found
+app.use((req, res) =>
+  res.status(404).json({ success: false, message: "Not found" })
+);
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(DB);
+    app.listen(PORT, () => console.log(`Server is listening to port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
