43a9a8ffb25dc35a99355c46f43a56ff6ba2761d
aramind <88814622+aramind@users.noreply.github.com>
2024-11-24 18:29:56 +0800

    feat: adding middle ware to verify jwt

---
+++ server/src/middlewares/verifyJWT.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const sendResponse = require("../utils/senResponse");
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization || req.headers.Authorization;
+
+  if (!authHeader?.startsWith("Bearer")) {
+    return sendResponse.failed(res, "Unknown Authorization", null, 401);
+  } else {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.AUTH_ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        if (err.message === "jwt expired") {
+          console.log("Refreshing...");
+        }
+        return sendResponse.failed(res, "Unauthorize access", err, 403);
+      }
+
+      req.credentials = decoded.HouseInfo;
+      // TODO: remove if ok na
+      console.log("in VERIFYING JWT, CREDENTIALS:", req.credentials);
+      next();
+    });
+  }
+};
+
+module.exports = verifyJWT;
