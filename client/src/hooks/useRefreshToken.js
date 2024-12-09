d88506c0e635068af9d3055f2090f1f8c58b5c6d
aramind <88814622+aramind@users.noreply.github.com>
2024-12-09 23:03:08 +0800

    feat: adding hook to handle refreshing

---
+++ client/src/hooks/useRefreshToken.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import useAuth from "./useAuth";
+
+const url = `${process.env.REACT_APP_API_URL}/v1/refresh`;
+
+const useRefreshToken = () => {
+  const { setAuth } = useAuth();
+
+  const refresh = async () => {
+    const response = await axios.get(url, { withCredentials: true });
+
+    const userInfo = response?.data?.data;
+
+    const newAccessToken = response?.data?.data?.token;
+
+    setAuth((prev) => {
+      return { ...prev, ...userInfo, token: newAccessToken };
+    });
+
+    return newAccessToken;
+  };
+
+  return refresh;
+};
+
+export default useRefreshToken;
