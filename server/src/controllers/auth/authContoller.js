8d84893256cf4c12a2a8cfef0a7044ea5626a7af
aramind <88814622+aramind@users.noreply.github.com>
2024-12-12 13:42:20 +0800

    feat: adding controller to handle refreshing accesstoken

--- server/src/controllers/auth/authContoller.js
+++ server/src/controllers/auth/authContoller.js
@@ -1,9 +1,11 @@
 const login = require("./login");
+const refresh = require("./refresh");
 const signup = require("./signup");
 
 const authController = {
   signup,
   login,
+  refresh,
 };
 
 module.exports = authController;
---
+++ server/src/controllers/auth/refresh.js
@@ -0,0 +1,62 @@
+const House = require("../../models/House");
+const sendResponse = require("../../utils/senResponse");
+const _ = require("lodash");
+const jwt = require("jsonwebtoken");
+const generateAccessToken = require("../../utils/generateAccessToken");
+
+const refresh = async (req, res) => {
+  try {
+    const refreshToken = req.cookies?.jwt;
+
+    if (!refreshToken) {
+      return sendResponse.failed(res, "Unauthorized", null, 401);
+    }
+
+    const house = await House.findOne({
+      refreshToken,
+    });
+
+    if (!house) {
+      return sendResponse.failed(res, "Unrecognized credentials", null, 404);
+    }
+
+    const returnedHouseInfo = _.pick(house, [
+      "_id",
+      "name",
+      "address",
+      "houseType",
+      "picture",
+      "membershipType",
+      "membershipStatus",
+    ]);
+
+    jwt.verify(
+      refreshToken,
+      process.env.AUTH_REFRESH_TOKEN_SECRET,
+      (err, decoded) => {
+        if (err) {
+          throw err;
+        } else {
+          if (house?._id?.toString() !== decoded.HouseInfo._id) {
+            return sendResponse.failed(res, "Unauthorized", null, 400);
+          }
+          return sendResponse.success(
+            res,
+            "Refreshing successful",
+            {
+              ...returnedHouseInfo,
+              houseInfo: returnedHouseInfo,
+              token: generateAccessToken(house),
+            },
+            200
+          );
+        }
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    return sendResponse.failed(res, "Error in refreshing credentials", 500);
+  }
+};
+
+module.exports = refresh;
