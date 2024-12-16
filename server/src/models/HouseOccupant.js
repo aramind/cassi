42cbbddc7f980382dc1b3ef87588521435a76320
aramind <88814622+aramind@users.noreply.github.com>
2024-12-16 23:49:52 +0800

    feat: adding fields to HouseOccupant model

--- server/src/models/HouseOccupant.js
+++ server/src/models/HouseOccupant.js
@@ -1,3 +1,45 @@
-const HouseOccupantSchema = new Schema({});
+const { Schema } = require("mongoose");
+const House = require("./House");
+const Occupant = require("./Occupant");
+
+const HouseOccupantSchema = new Schema({
+  houseId: {
+    type: Schema.Types.ObjectId,
+    ref: House,
+    required: true,
+  },
+  occupantId: {
+    type: Schema.Types.ObjectId,
+    ref: Occupant,
+    required: true,
+  },
+  moveInDate: {
+    type: Date,
+    require: true,
+    default: Date.now(),
+  },
+
+  moveOutDate: {
+    type: Date,
+  },
+
+  type: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  version: {
+    type: String,
+    required: true,
+    default: CONSTANTS?.DEFAULT_VALUES?.version,
+    enum: { values: CONSTANTS?.VERSIONS },
+  },
+});
 
 module.exports = mongoose.model("HouseOccupant", HouseOccupantSchema);
