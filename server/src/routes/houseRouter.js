6429145c273959ac920658bb0f4bd2401e371032
aramind <88814622+aramind@users.noreply.github.com>
2024-12-27 23:11:55 +0800

    feat: applying verifying jwt for house related routes

--- server/src/routes/houseRouter.js
+++ server/src/routes/houseRouter.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const houseController = require("../controllers/house/houseController");
 const router = express.Router();
+const verifyJWT = require("../middlewares/verifyJWT");
 
+router.use(verifyJWT);
 router.get("/profile/:houseId", houseController.getHouseProfile);
 
 module.exports = router;
