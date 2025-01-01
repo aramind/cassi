4c4760102e48463dba620a51ddc5f2e2b1f0a038
aramind <88814622+aramind@users.noreply.github.com>
2025-01-01 19:56:26 +0800

    minor: adding note

--- server/src/routes/houseOccupantRouter.js
+++ server/src/routes/houseOccupantRouter.js
@@ -3,6 +3,8 @@ const verifyJWT = require("../middlewares/verifyJWT");
 const houseOccupantController = require("../controllers/houseOccupants/houseOccupantController");
 const router = express.Router();
 
+//url: root/v1/house-occupant
+
 console.log("IN HO ROUTER");
 router.use(verifyJWT);
 router.post("/add", houseOccupantController.addHouseOccupant);
