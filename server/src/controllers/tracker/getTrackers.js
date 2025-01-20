573146014f709898e4e1d23d199045a0b5a263d3
aramind <88814622+aramind@users.noreply.github.com>
2025-01-20 16:39:28 +0800

    feat: creating controller to handle getting tracker(s)

---
+++ server/src/controllers/tracker/getTrackers.js
@@ -0,0 +1,31 @@
+const Tracker = require("../../models/Tracker");
+const sendResponse = require("../../utils/senResponse");
+
+const getTrackers = async (req, res) => {
+  try {
+    const { fields, ...queryParams } = req.query;
+    const requestedFields = fields?.length > 0 ? fields.split(" , ") : [];
+
+    const trackers = await Tracker.find(queryParams, requestedFields.join(" "));
+
+    if (!trackers) {
+      return sendResponse.failed(res, "Tracker(s) not found", null, 404);
+    }
+    return sendResponse.success(
+      res,
+      "Tracker(s) successfully retrieved",
+      trackers,
+      200
+    );
+  } catch (error) {
+    console.log(error);
+    return sendResponse.failed(
+      res,
+      "Server Error: Retrieving tracker(s)",
+      error,
+      500
+    );
+  }
+};
+
+module.exports = getTrackers;
