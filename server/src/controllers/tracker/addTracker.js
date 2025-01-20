c768bab88ca8824af1bd0afb252426808bffbe51
aramind <88814622+aramind@users.noreply.github.com>
2025-01-20 16:16:28 +0800

    feat: creating controller to handle adding new tracker

---
+++ server/src/controllers/tracker/addTracker.js
@@ -0,0 +1,34 @@
+const Tracker = require("../../models/Tracker");
+const sendResponse = require("../../utils/senResponse");
+
+const addTracker = async (req, res) => {
+  try {
+    const { tracker } = req.body;
+    const houseId = req?.credentials?._id;
+
+    const existingTracker = await Tracker.findOne({
+      title: tracker?.title,
+    });
+
+    if (existingTracker) {
+      return sendResponse.failed(res, "Tracker already exists!", null, 409);
+    }
+
+    const newTracker = await Tracker.create({
+      ...tracker,
+      house: houseId,
+    });
+
+    return sendResponse.success(res, "New tracker added", newTracker, 201);
+  } catch (error) {
+    console.log(error);
+    return sendResponse.failed(
+      res,
+      "Server Error: Adding new tracker",
+      error,
+      500
+    );
+  }
+};
+
+module.exports = addTracker;
