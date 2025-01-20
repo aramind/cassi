6d207b7efbfd220db07db9f8f3e9a07ad0669bfe
aramind <88814622+aramind@users.noreply.github.com>
2025-01-20 18:24:37 +0800

    feat: adding tracker url

--- client/src/constants/urls.js
+++ client/src/constants/urls.js
@@ -4,6 +4,7 @@ const urls = {
   ROOT: `${baseUrl}/v1`,
   HOUSE: `${baseUrl}/v1/house`,
   HOUSEOCCUPANT: `${baseUrl}/v1/house-occupant`,
+  TRACKERS: `${baseUrl}/v1/trackers`,
 };
 
 export default urls;
