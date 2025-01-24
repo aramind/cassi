38b0e3d9c6607a2a93b3a04de4e8610d73b7c565
aramind <88814622+aramind@users.noreply.github.com>
2025-01-24 23:00:37 +0800

    feat: adding update tracker controller

---
+++ server/src/controllers/tracker/updateTracker.js
@@ -0,0 +1,36 @@
+const Tracker = require("../../models/Tracker");
+const sendResponse = require("../../utils/senResponse");
+
+const updateTracker = async (req, res) => {
+  try {
+    const { trackerId } = req.params;
+    const { data } = req.body;
+
+    const updatedTracker = await Tracker.findOneAndUpdate(
+      { _id: trackerId },
+      { $set: data },
+      { new: true }
+    );
+
+    if (!updatedTracker) {
+      return sendResponse.failed(res, "Tracker not found!", null, 404);
+    }
+
+    return sendResponse.success(
+      res,
+      "Tracker updated successfully!",
+      updatedTracker,
+      200
+    );
+  } catch (error) {
+    console.log(error);
+    return sendResponse.failed(
+      res,
+      "Server Error: Updating tracker",
+      error,
+      500
+    );
+  }
+};
+
+module.exports = updateTracker;
