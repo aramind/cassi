4461d2731e82af9c7920f5782a1edbbbd4e364dd
aramind <88814622+aramind@users.noreply.github.com>
2025-03-27 08:09:03 +0800

    feat: creating a context for providing info related to house in general

--- client/src/context/HouseProvider.js
+++ client/src/context/HouseProvider.js
@@ -1,52 +1,31 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useMemo } from "react";
 import LoadingPage from "../pages/LoadingPage";
 import ErrorPage from "../pages/ErrorPage";
 import useAuth from "../hooks/useAuth";
-import useApiGet from "../hooks/api/useApiGet";
-import useHouseReq from "../hooks/api/authenticated/useHouseReq";
 
-const defaultContextValue = {};
-export const HouseContext = createContext(defaultContextValue);
+export const HouseContext = createContext();
 
 const HouseProvider = ({ children }) => {
   const { auth } = useAuth();
 
-  const { getHouseProfile } = useHouseReq({ isPublic: false, showAck: false });
-  const [houseProfile, setHouseProfile] = useState([]);
-
-  //   const {
-  //     data: houseProfileData,
-  //     isLoading: isLoadingInGettingHouseProfile,
-  //     isError: isErrorInGettingHouseProfile,
-  //     error: errorInGettingHouseProfile,
-  //   } = useApiGet("house-profile", () => getHouseProfile(auth?._id), {
-  //     refetchOnWindowFocus: true,
-  //     retry: 3,
-  //     enabled: !!auth?._id,
-  //   });
-
-  const { data, isError } = useApiGet("houseProfile", () =>
-    getHouseProfile(auth?._id)
+  const occupantOptions = useMemo(
+    () =>
+      auth?.houseInfo?.houseOccupants?.map((ho) => {
+        return {
+          id: ho._id,
+          name: ho?.occupant?.name?.nickName || ho?.occupant?.name.firstName,
+        };
+      }),
+    [auth?.houseInfo?.houseOccupants]
   );
-  //   useEffect(() => {
-  //     setHouseProfile(houseProfileData?.data);
-  //   }, [houseProfileData?.data]);
-  //   const isLoading = isLoadingInGettingHouseProfile;
-  //   const hasError = isErrorInGettingHouseProfile;
-  //   const getErrorMessage = () => {
-  //     const error = errorInGettingHouseProfile;
-  //     return (
-  //       error?.message ||
-  //       "An error occurred! Please try again doing the previous action."
-  //     );
-  //   };
+
   const isLoading = false;
   const hasError = false;
   const getErrorMessage = () => {
     return "An error occurred! Please try again doing the previous action.";
   };
   return (
-    <HouseContext.Provider value={{}}>
+    <HouseContext.Provider value={{ occupantOptions }}>
       {isLoading ? (
         <LoadingPage />
       ) : hasError ? (
