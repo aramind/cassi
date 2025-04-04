626526ca272b29f20f270a046a69ab62110e3c45
aramind <88814622+aramind@users.noreply.github.com>
2025-04-04 14:48:06 +0800

    refactor: restyling deleted trackers to be displayed on dialog

--- client/src/pages/trackers/DeletedTrackers.jsx
+++ client/src/pages/trackers/DeletedTrackers.jsx
@@ -1,37 +1,38 @@
-import { Box, IconButton, Stack, Typography } from "@mui/material";
+import { Box, Divider, IconButton, Stack, Typography } from "@mui/material";
 import React from "react";
 import RestoreOutlinedIcon from "@mui/icons-material/RestoreOutlined";
 
 const DeletedTrackers = ({ trackers, restoreTrackerHandler }) => {
   return (
-    <Stack spacing={1} width={1}>
-      <Typography textAlign="center">DELETED TRACKERS</Typography>
+    <Stack width={1}>
       {trackers?.map((tracker, index) => {
         return (
-          <Stack
-            width={1}
-            justifyContent="space-between"
-            className="outlined"
-            direction="row"
-            p={0.5}
-          >
-            <Stack>
-              <Typography variant="h6">{tracker?.title}</Typography>
-              <Typography color="secondary" ml={2}>
-                {tracker?.description}
-              </Typography>
+          <>
+            <Stack
+              width={1}
+              justifyContent="space-between"
+              direction="row"
+              p={0.5}
+            >
+              <Stack>
+                <Typography variant="h6">{tracker?.title}</Typography>
+                <Typography color="secondary" ml={2}>
+                  {tracker?.description}
+                </Typography>
+              </Stack>
+              <Box>
+                <IconButton
+                  aria-label="restore"
+                  onClick={() =>
+                    restoreTrackerHandler(tracker?._id, tracker?.title)
+                  }
+                >
+                  <RestoreOutlinedIcon />
+                </IconButton>
+              </Box>
             </Stack>
-            <Box>
-              <IconButton
-                aria-label="restore"
-                onClick={() =>
-                  restoreTrackerHandler(tracker?._id, tracker?.title)
-                }
-              >
-                <RestoreOutlinedIcon />
-              </IconButton>
-            </Box>
-          </Stack>
+            <Divider />
+          </>
         );
       })}
     </Stack>
