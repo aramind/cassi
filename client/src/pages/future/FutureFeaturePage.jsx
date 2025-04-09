3ce163dce214b3b1b4542d0758175ba65a0e46f0
aramind <88814622+aramind@users.noreply.github.com>
2025-04-09 15:11:29 +0800

    style: styling future page

--- client/src/pages/future/FutureFeaturePage.jsx
+++ client/src/pages/future/FutureFeaturePage.jsx
@@ -4,6 +4,7 @@ import { Button, Stack, Typography } from "@mui/material";
 import BuildRoundedIcon from "@mui/icons-material/BuildRounded";
 import ReplyRoundedIcon from "@mui/icons-material/ReplyRounded";
 import { useNavigate } from "react-router-dom";
+import HeroImage from "../login-register/HeroImage";
 
 const FutureFeaturePage = () => {
   const navigate = useNavigate();
@@ -16,12 +17,20 @@ const FutureFeaturePage = () => {
         alignItems="center"
         flex={1}
       >
+        <HeroImage
+          bgcolor={(theme) => theme.palette?.accent?.light}
+          width={{ xs: "150px", md: "300px" }}
+          height={{ xs: "150px", md: "300px" }}
+          src={`/assets/images/unboxing.png`}
+          imageWidth={`70vw`}
+        />
+        <br />
         <Typography variant="h5" color="error" textAlign="center" width={1}>
           Oops!
         </Typography>
         <Typography variant="h6" textAlign="center" width={1}>
           You're a bit early — we're still building this feature!{" "}
-          <BuildRoundedIcon fontSize="inherit" />
+          <BuildRoundedIcon fontSize="inherit" color="inherit" />
         </Typography>
         <br />
         <Button
