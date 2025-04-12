1f77e41fdb57027ad130d0deecafd3b0f9bfd8f6
aramind <88814622+aramind@users.noreply.github.com>
2025-04-12 15:56:16 +0800

    feat: adding component to handle adding/updating announcement in a dialog

---
+++ client/src/pages/announcements/AnnouncementDialog.jsx
@@ -0,0 +1,137 @@
+import React from "react";
+import useConfirmActionDialog from "../../hooks/useConfirmActionDialog";
+import { useForm } from "react-hook-form";
+import {
+  Button,
+  DialogActions,
+  DialogContent,
+  Stack,
+  Typography,
+} from "@mui/material";
+import DraggableDialog from "../../components/DraggableDialog";
+import FormWrapper from "../../wrappers/FormWrapper";
+import ControlledLabelledTextField from "../../components/controlled/ControlledLabelledTextField";
+import { DevTool } from "@hookform/devtools";
+
+const getTitle = (action) => {
+  let title = "";
+
+  switch (action) {
+    case "add": {
+      title = "Add New Announcement";
+      break;
+    }
+    case "update": {
+      title = "Update Announcement";
+      break;
+    }
+    default:
+      title = "";
+  }
+
+  return title;
+};
+
+const getSubmitBtnText = (action) => {
+  let text = "";
+
+  switch (action) {
+    case "add": {
+      text = "Submit";
+      break;
+    }
+    case "update": {
+      text = "Update";
+      break;
+    }
+    default:
+      text = "";
+  }
+
+  return text;
+};
+
+const AnnouncementDialog = ({ open, setOpen, data, action, submitHandler }) => {
+  const { handleOpen: handleConfirm, renderConfirmActionDialog } =
+    useConfirmActionDialog();
+
+  // form related
+  const {
+    control,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onTouched",
+    defaultValues: action === "add" ? {} : data || {},
+  });
+
+  const formMethods = {
+    control,
+    handleSubmit,
+    errors,
+  };
+
+  //   handlers
+  const onSubmit = async (formData) => {
+    console.log("SUBMITTING: ", formData);
+    setOpen(false);
+  };
+
+  const handleClose = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setOpen(false);
+  };
+
+  const handleConfirmClear = () => {
+    handleConfirm(
+      "Confirm Clear",
+      <Typography>Are you sure you want to reset all fields</Typography>,
+      () => reset()
+    );
+  };
+
+  let title = getTitle(action);
+  let submitBtnText = getSubmitBtnText(action);
+
+  return (
+    <>
+      <DraggableDialog open={open} handleClose={handleClose} title={title}>
+        <DialogContent>
+          <FormWrapper formMethods={formMethods}>
+            <form noValidate>
+              <Stack spacing={1}>
+                <ControlledLabelledTextField
+                  id="announcement-id"
+                  label="title"
+                  name="title"
+                />
+                <ControlledLabelledTextField
+                  id="announcement-description"
+                  label="description"
+                  name="description"
+                  multiline
+                  tfProps={{ rows: 4 }}
+                />
+              </Stack>
+            </form>
+          </FormWrapper>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button className="outlined" onClick={handleConfirmClear}>
+            Reset
+          </Button>
+          <Button className="contained" onClick={handleSubmit(onSubmit)}>
+            {submitBtnText}
+          </Button>
+        </DialogActions>
+      </DraggableDialog>
+      {renderConfirmActionDialog()}
+      <DevTool control={control} />
+    </>
+  );
+};
+
+export default AnnouncementDialog;
