700da9d3be8a0523b31871321f1cc0f184afed99
aramind <88814622+aramind@users.noreply.github.com>
2025-04-12 15:05:32 +0800

    fix: typo error

--- server/src/routes/announcementRouter.js
+++ server/src/routes/announcementRouter.js
@@ -2,7 +2,6 @@ const express = require("express");
 const verifyJWT = require("../middlewares/verifyJWT");
 const verifyHouse = require("../middlewares/verifyHouse");
 const announcementController = require("../controllers/announcements/announcementController");
-const { model } = require("mongoose");
 const router = express.Router();
 
 router.use(verifyJWT);
@@ -10,4 +9,4 @@ router.use(verifyHouse);
 console.log("IN ANNOUNCEMENT CONTROLLER");
 router.post("/add", announcementController.add);
 
-model.exports = router;
+module.exports = router;
