d1c7d6aec939c6fdcef147153f060bda5a7c0682
aramind <88814622+aramind@users.noreply.github.com>
2025-04-13 23:20:32 +0800

    feat: adding controller to get announcments given queries

---
+++ server/src/controllers/announcements/getAnnouncements.js
@@ -0,0 +1,36 @@
+const Announcement = require("../../models/Announcement");
+const sendResponse = require("../../utils/senResponse");
+
+const getAnnouncements = async (req, res) => {
+  try {
+    const { fields, ...queryParams } = req.query;
+
+    const requestedFields = fields ? fields.split(",").join(" ").trim() : null;
+
+    const announcements = await Announcement.find(
+      queryParams,
+      requestedFields || undefined
+    );
+
+    if (!announcements) {
+      return sendResponse.failed(res, "Announcement(s) not found!", null, 404);
+    }
+
+    return sendResponse.success(
+      res,
+      "Announcement(s) successfully retrieved",
+      announcements,
+      200
+    );
+  } catch (error) {
+    console.log(error);
+    return sendResponse.failed(
+      res,
+      "Server Error: Retrieving announcement(s)",
+      error,
+      500
+    );
+  }
+};
+
+module.exports = getAnnouncements;
