58b3851c7a4d14870575491af75bddc576b92d3c
aramind <88814622+aramind@users.noreply.github.com>
2025-04-14 22:04:03 +0800

    fix: navdial blocking components behind id even if navdial is hidden

--- client/src/components/speed-dial/NavDial.jsx
+++ client/src/components/speed-dial/NavDial.jsx
@@ -37,51 +37,53 @@ const NavDial = () => {
       sx={{ position: "fixed", bottom: 16, right: 16, zIndex: 1300 }}
     >
       {/* Action Grid */}
-      <Zoom in={open}>
-        <Box
-          sx={{
-            position: "relative",
-            display: "grid",
-            gridTemplateColumns: "repeat(3, 56px)",
-            gap: 1,
-            justifyContent: "end",
-            mb: 1,
-            borderRadius: 6,
-            p: 2,
-          }}
-        >
+      {open && (
+        <Zoom in={open}>
           <Box
             sx={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-              backgroundColor: (theme) => theme.palette.primary.light,
-              opacity: 0.3, // Opacity only for background
+              position: "relative",
+              display: "grid",
+              gridTemplateColumns: "repeat(3, 56px)",
+              gap: 1,
+              justifyContent: "end",
+              mb: 1,
               borderRadius: 6,
-              zIndex: -1, // Ensure it's behind the content
+              p: 2,
             }}
-          />
-          {links.map((link) => (
-            <Tooltip key={link.name} title={link.name} placement="top">
-              <Fab
-                size="small"
-                onClick={() => {
-                  // setOpen(false);
-                  navigate(link.link);
-                }}
-                sx={{
-                  bgcolor: (theme) => theme.palette.accent.light,
-                  opacity: 0.8,
-                }}
-              >
-                {link.icon}
-              </Fab>
-            </Tooltip>
-          ))}
-        </Box>
-      </Zoom>
+          >
+            <Box
+              sx={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                backgroundColor: (theme) => theme.palette.primary.light,
+                opacity: 0.3, // Opacity only for background
+                borderRadius: 6,
+                zIndex: -1, // Ensure it's behind the content
+              }}
+            />
+            {links.map((link) => (
+              <Tooltip key={link.name} title={link.name} placement="top">
+                <Fab
+                  size="small"
+                  onClick={() => {
+                    // setOpen(false);
+                    navigate(link.link);
+                  }}
+                  sx={{
+                    bgcolor: (theme) => theme.palette.accent.light,
+                    opacity: 0.8,
+                  }}
+                >
+                  {link.icon}
+                </Fab>
+              </Tooltip>
+            ))}
+          </Box>
+        </Zoom>
+      )}
 
       {/* Main FAB */}
       <Stack justifyContent="flex-end" direction="row">
