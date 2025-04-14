4b7a35ec7b836f8bb6f4fd81d9f17280c1ed51a6
aramind <88814622+aramind@users.noreply.github.com>
2025-04-14 12:51:19 +0800

    feat: initial layout of chips in the AnnouncementCard

--- client/src/pages/announcements/AnnouncementCard.jsx
+++ client/src/pages/announcements/AnnouncementCard.jsx
@@ -1,17 +1,41 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Chip, Stack, Typography } from "@mui/material";
 import React from "react";
 
 const AnnouncementCard = ({ announcement }) => {
+  console.log(announcement);
   return (
-    <Box
+    <Stack
       width="40vw"
-      height="40vw"
+      //   minHeight="200px"
+      height="auto"
       maxWidth="400px"
-      maxHeight="400px"
       className="outlined"
+      p={1}
+      borderRadius={2}
+      sx={{
+        aspectRatio: "1 / 1", // height = width
+      }}
     >
-      <Typography>{announcement?.title}</Typography>
-    </Box>
+      <Stack
+        direction="row"
+        gap={1}
+        justifyContent="flex-start"
+        width={1}
+        flexWrap="wrap"
+      >
+        <Chip color="primary" label={announcement?.status} size="small" />
+        <Chip color="info" label={announcement?.type} size="small" />
+        <Chip
+          sx={{ bgcolor: (theme) => theme.palette.accent.light }}
+          label={announcement?.importance}
+          size="small"
+        />
+      </Stack>
+      <Typography variant="h6">{announcement?.title}</Typography>
+      <Typography variant="body1">{announcement?.content}</Typography>
+      <Typography variant="body1">{announcement?.createdBy}</Typography>
+      <Typography variant="body1">{announcement?.createdAt}</Typography>
+    </Stack>
   );
 };
 
