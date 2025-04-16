1c26cca2f5126628111a0cb046434f5d6e3306ca
aramind <88814622+aramind@users.noreply.github.com>
2025-04-16 23:29:35 +0800

    feat: styling the header displayed for each task

--- client/src/pages/tasks/TasksContainer.jsx
+++ client/src/pages/tasks/TasksContainer.jsx
@@ -3,12 +3,21 @@ import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
+  Chip,
   Stack,
   Typography,
 } from "@mui/material";
 import ExpandMoreRoundedIcon from "@mui/icons-material/ExpandMoreRounded";
+import SignalCellularAlt1BarRoundedIcon from "@mui/icons-material/SignalCellularAlt1BarRounded";
+import SignalCellularAlt2BarRoundedIcon from "@mui/icons-material/SignalCellularAlt2BarRounded";
+import SignalCellularAltRoundedIcon from "@mui/icons-material/SignalCellularAltRounded";
 import TaskDetails from "./TaskDetails";
 
+const PRIORITY_ICONS = {
+  //   low: <SignalCellularAlt1BarRoundedIcon />,
+  //   medium: <SignalCellularAlt2BarRoundedIcon />,
+  //   high: <SignalCellularAltRoundedIcon />,
+};
 const TasksContainer = ({ tasks }) => {
   console.log(tasks);
 
@@ -16,14 +25,41 @@ const TasksContainer = ({ tasks }) => {
   return (
     <Stack width={1} px={2} my={2}>
       {tasks?.map((t, i) => (
-        <Accordion>
+        <Accordion key={i}>
           <AccordionSummary
             expandIcon={<ExpandMoreRoundedIcon />}
             aria-controls={`panel${i}-content`}
             id={`panel${i}-header`}
-            key={i}
           >
-            <Typography variant="h6">{t?.title}</Typography>
+            <Stack
+              spacing={1}
+              direction="row"
+              flexWrap="wrap"
+              justifyContent="flex-start"
+              //   className="outlined"
+              width={1}
+              //   py={1}
+            >
+              <Typography variant="h6">{t?.title?.toUpperCase()}</Typography>
+              {t?.status && (
+                <Chip
+                  variant="outlined"
+                  color="info"
+                  //   sx={{ bgcolor: importanceColor[announcement?.importance] }}
+                  label={t?.status?.toUpperCase()}
+                  size="small"
+                />
+              )}
+              {t?.priority && (
+                <Chip
+                  variant="outlined"
+                  color="error"
+                  //   sx={{ bgcolor: importanceColor[announcement?.importance] }}
+                  label={t?.priority?.toUpperCase()}
+                  size="small"
+                />
+              )}
+            </Stack>
           </AccordionSummary>
           <AccordionDetails>
             <TaskDetails task={t} />
