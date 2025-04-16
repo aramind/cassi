557b8b1a663740dfac28c176f5dec4800743a47f
aramind <88814622+aramind@users.noreply.github.com>
2025-04-16 14:17:16 +0800

    clean: removing console logs

--- server/src/routes/announcementRouter.js
+++ server/src/routes/announcementRouter.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.use(verifyJWT);
 router.use(verifyHouse);
-console.log("IN ANNOUNCEMENT CONTROLLER");
+
 router.post("", announcementController.add);
 router.get("/:id", announcementController.getById);
 router.get("", announcementController.getAnnouncements);
