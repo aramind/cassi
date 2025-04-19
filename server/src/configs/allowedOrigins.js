08396f8abae184f38c1e4781f59cf032c2b12a88
aramind <88814622+aramind@users.noreply.github.com>
2025-04-19 23:32:49 +0800

    refactor: allowing the live site to access the be

--- server/src/configs/allowedOrigins.js
+++ server/src/configs/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:3000", "http://localhost:5000"];
+const allowedOrigins = ["http://localhost:3000", "http://localhost:5000", "https://cassi-fbe1.onrender.com"];
 
 module.exports = allowedOrigins;
