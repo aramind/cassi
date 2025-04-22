6f0e985c5fd996b9d25667708005fe23597e48da
aramind <88814622+aramind@users.noreply.github.com>
2025-04-22 22:41:32 +0800

    feat: creating util function to supply the confirm text

--- client/src/utils/dialogUtils.js
+++ client/src/utils/dialogUtils.js
@@ -1,5 +1,18 @@
+import { Typography } from "@mui/material";
+
 export const dialogUtils = {
   getDialogTitle: (action = "", item = "") => {
     return `${action}${action === "add" ? " new " : " "}${item}`.toUpperCase();
   },
+};
+
+export const getConfirmText = (action, item) => {
+  const message = {
+    add: `Continue adding this ${item}?`,
+    update: `Continue updating this ${item}?`,
+    delete: `Are you sure you want to delete this ${item}`,
+  };
+
+  let text = message[action] || "Continue?";
+  return <Typography>{text.toUpperCase()}</Typography>;
 };
