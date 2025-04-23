9fe8c52f472c284d3cdb8f8143ad48fb8b224704
aramind <88814622+aramind@users.noreply.github.com>
2025-04-23 23:42:36 +0800

    feat: adding the feedback notif component to main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import { CssBaseline } from "@mui/material";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes";
+import FeedbackNotif from "./components/notifications/FeedbackNotif";
 
 function App() {
   return (
     <>
       <CssBaseline />
       <RouterProvider router={routes.router} />
+      <FeedbackNotif />
     </>
   );
 }
