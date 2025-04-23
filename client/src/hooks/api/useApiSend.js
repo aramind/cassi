831b44bcd2c00f57ca9ec2899c9e1b778040d5b3
aramind <88814622+aramind@users.noreply.github.com>
2025-04-23 16:09:39 +0800

    refactor: (major) using mutateAsync in the useApiSend

--- client/src/hooks/api/useApiSend.js
+++ client/src/hooks/api/useApiSend.js
@@ -1,22 +1,60 @@
 import { useMutation, useQueryClient } from "react-query";
 
-const useApiSend = (fn, invalidateKey, successFn, errorFn, options) => {
+const useApiSend = (fn, invalidateKey, options) => {
   const queryClient = useQueryClient();
 
-  return useMutation({
-    mutationFn: fn,
-    onSuccess: async (data) => {
+  const mutation = useMutation({ mutationFn: fn, retry: 0, ...options });
+
+  const send = async (
+    variables,
+    { showSuccess = true, showError = true } = {}
+  ) => {
+    try {
+      const res = await mutation.mutateAsync(variables);
+
       if (invalidateKey) {
         invalidateKey.forEach((key) => queryClient.invalidateQueries(key));
       }
-      successFn && successFn(data);
-    },
-    onError: async (err) => {
-      errorFn && errorFn(err);
-    },
-    retry: 0,
-    ...options,
-  });
+
+      if (showSuccess) {
+        alert(
+          typeof showSuccess === "string" ? showSuccess : "Operation successful"
+        );
+      }
+
+      return res;
+    } catch (error) {
+      if (showError) {
+        alert(
+          typeof showError === "string"
+            ? showError
+            : `Request failed: ${error?.message}`
+        );
+      }
+      throw error;
+    }
+  };
+
+  return {
+    send,
+    isLoading: mutation.isLoading,
+    isError: mutation.isError,
+    error: mutation.error,
+  };
+  // return useMutation({
+  //   mutationFn: fn,
+  //   onSuccess: async (data) => {
+  //     if (invalidateKey) {
+  //       invalidateKey.forEach((key) => queryClient.invalidateQueries(key));
+  //     }
+  //     successFn && successFn(data);
+  //   },
+  //   onError: async (err) => {
+  //     errorFn && errorFn(err);
+  //   },
+  //   retry: 0,
+  //   ...options,
+  // });
 };
 
 export default useApiSend;
