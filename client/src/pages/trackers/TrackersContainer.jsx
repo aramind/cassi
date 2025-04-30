6c94a26a5a844b2a4d0545c445e65a15ed4c156a
aramind <88814622+aramind@users.noreply.github.com>
2025-04-30 14:51:02 +0800

    refactor: extracting each accordion to separate component

--- client/src/pages/trackers/TrackersContainer.jsx
+++ client/src/pages/trackers/TrackersContainer.jsx
@@ -11,6 +11,8 @@ import { ExpandMoreIcon } from "../../utils/muiIcons";
 import TrackerActionsGroup from "./TrackerActionsGroup";
 import TrackerEntries from "./TrackerEntries";
 import MyButton from "../../components/buttons/MyButton";
+import useAppState from "../../hooks/useAppState";
+import TrackerAccordion from "./TrackerAccordion";
 
 const TrackersContainer = ({
   trackers,
@@ -26,58 +28,15 @@ const TrackersContainer = ({
   return (
     <Box width={1} my={2}>
       {trackers.map((tracker, i) => (
-        <Accordion key={tracker?._id}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls={`panel${i}-content`}
-            id={`panel${i}-header`}
-          >
-            <Stack direction="row" alignItems="center" spacing={2}>
-              <TrackerActionsGroup
-                tracker={tracker}
-                updateHandler={() => handleOpenDialog("update", tracker)}
-                deleteHandler={() =>
-                  handleDeletingTrackerInfo(tracker, { status: "deleted" })
-                }
-              />
-              <Stack width={1} alignItems="flex-start">
-                <Typography
-                  variant="h5"
-                  width={1}
-                  textAlign="left"
-                  color="primary.dark"
-                >
-                  {tracker?.title}
-                </Typography>
-                <Typography
-                  variant="h6"
-                  fontWeight="normal"
-                  textAlign="left"
-                  width={1}
-                >
-                  {tracker?.description}
-                </Typography>
-              </Stack>
-            </Stack>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Stack justifyContent="center" alignItems="center" spacing={2}>
-              <TrackerEntries
-                tracker={tracker}
-                handleConfirmDeleteEntry={handleConfirmDeleteEntry}
-                handleOpenEntryDialog={handleOpenEntryDialog}
-              />
-              <MyButton
-                type="primary"
-                text="add entry"
-                variant="contained"
-                onClickHandler={() =>
-                  handleOpenEntryDialog("add", { tracker, entry: null })
-                }
-              />
-            </Stack>
-          </AccordionDetails>
-        </Accordion>
+        <TrackerAccordion
+          key={tracker._id}
+          tracker={tracker}
+          index={i}
+          handleDeletingTrackerInfo={handleDeletingTrackerInfo}
+          handleOpenDialog={handleOpenDialog}
+          handleConfirmDeleteEntry={handleConfirmDeleteEntry}
+          handleOpenEntryDialog={handleOpenEntryDialog}
+        />
       ))}
     </Box>
   );
