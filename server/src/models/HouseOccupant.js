34e70649ceab7eca6e9d799713a0ee3ff47f0f71
aramind <88814622+aramind@users.noreply.github.com>
2025-05-10 11:52:06 +0800

    refactor: adding timestamps option

--- server/src/models/HouseOccupant.js
+++ server/src/models/HouseOccupant.js
@@ -4,48 +4,43 @@ const Schema = mongoose.Schema;
 const House = require("./House");
 const Occupant = require("./Occupant");
 
-const HouseOccupantSchema = new Schema({
-  house: {
-    type: Schema.Types.ObjectId,
-    ref: House,
-    required: true,
+const HouseOccupantSchema = new Schema(
+  {
+    house: {
+      type: Schema.Types.ObjectId,
+      ref: House,
+      required: true,
+    },
+    occupant: {
+      type: Schema.Types.ObjectId,
+      ref: Occupant,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: CONSTANTS?.DEFAULT_VALUES?.houseOccupantStatus,
+      enum: CONSTANTS?.HOUSE_OCCUPANT_STATUSES,
+    },
+    moveInDate: {
+      type: Date,
+      require: true,
+      default: Date.now,
+    },
+    moveOutDate: {
+      type: Date,
+    },
+    type: {
+      type: String,
+    },
+    version: {
+      type: String,
+      required: true,
+      default: CONSTANTS?.DEFAULT_VALUES?.version,
+      enum: CONSTANTS?.VERSIONS,
+    },
   },
-  occupant: {
-    type: Schema.Types.ObjectId,
-    ref: Occupant,
-    required: true,
-  },
-  status: {
-    type: String,
-    required: true,
-    default: CONSTANTS?.DEFAULT_VALUES?.houseOccupantStatus,
-    enum: CONSTANTS?.HOUSE_OCCUPANT_STATUSES,
-  },
-  moveInDate: {
-    type: Date,
-    require: true,
-    default: Date.now(),
-  },
-  moveOutDate: {
-    type: Date,
-  },
-  type: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  version: {
-    type: String,
-    required: true,
-    default: CONSTANTS?.DEFAULT_VALUES?.version,
-    enum: CONSTANTS?.VERSIONS,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("HouseOccupant", HouseOccupantSchema);
