3aa0f3a71de345fc53a6f9716b2abaa8e9408c00
aramind <88814622+aramind@users.noreply.github.com>
2025-05-10 11:52:39 +0800

    refactor: allowing timestamps to auto handle updating of date

--- server/src/models/Occupant.js
+++ server/src/models/Occupant.js
@@ -2,57 +2,51 @@ const mongoose = require("mongoose");
 const CONSTANTS = require("../configs/constants");
 const Schema = mongoose.Schema;
 
-const OccupantSchema = new Schema({
-  name: {
-    firstName: { type: String, required: true },
-    middleName: { type: String },
-    lastName: { type: String, required: true },
-    nickName: { type: String },
+const OccupantSchema = new Schema(
+  {
+    name: {
+      firstName: { type: String, required: true },
+      middleName: { type: String },
+      lastName: { type: String, required: true },
+      nickName: { type: String },
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+    },
+    contactNumbers: {
+      type: [String],
+    },
+    gender: {
+      type: String,
+      enum: { values: CONSTANTS?.GENDER_OPTIONS },
+      default: CONSTANTS?.DEFAULT_VALUES?.gender,
+    },
+    occupation: {
+      type: String,
+    },
+    emergencyContact: {
+      name: { type: String },
+      address: { type: String },
+      relationToOccupant: { type: String },
+      email: { type: String, trim: true },
+      mobileNumber: { type: String },
+      phoneNumber: { type: String },
+    },
+    dateOfBirth: {
+      type: Date,
+    },
+    preferences: {
+      type: [String],
+    },
+    version: {
+      type: String,
+      default: CONSTANTS?.DEFAULT_VALUES?.version,
+      enum: { values: CONSTANTS?.VERSIONS },
+    },
   },
-  email: {
-    type: String,
-    trim: true,
-    lowercase: true,
-  },
-  contactNumbers: {
-    type: [String],
-  },
-  gender: {
-    type: String,
-
-    enum: { values: CONSTANTS?.GENDER_OPTIONS },
-    default: CONSTANTS?.DEFAULT_VALUES?.gender,
-  },
-  occupation: {
-    type: String,
-  },
-  emergencyContact: {
-    name: { type: String },
-    address: { type: String },
-    relationToOccupant: { type: String },
-    email: { type: String, trim: true },
-    mobileNumber: { type: String },
-    phoneNumber: { type: String },
-  },
-  dateOfBirth: {
-    type: Date,
-  },
-  preferences: {
-    type: [String],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-  version: {
-    type: String,
-    default: CONSTANTS?.DEFAULT_VALUES?.version,
-    enum: { values: CONSTANTS?.VERSIONS },
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Occupant", OccupantSchema);
