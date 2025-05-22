9678ed6c9fb126bbfde3021906847f1ecaf9174b
aramind <88814622+aramind@users.noreply.github.com>
2025-05-23 00:54:24 +0800

    feat: cycling with the diff large screen hero images

---
+++ client/src/constants/imageURLs.js
@@ -0,0 +1,9 @@
+export const largeScreenHeroImages = [
+  "home1.jpg",
+  "home2.jpg",
+  "home3.jpg",
+  "home4.jpg",
+  "home5.jpg",
+  "home6.jpg",
+  "home7.jpg",
+];
--- client/src/pages/login-register/LargeScreenHeroImage.jsx
+++ client/src/pages/login-register/LargeScreenHeroImage.jsx
@@ -1,12 +1,32 @@
 import { Box } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { largeScreenHeroImages } from "../../constants/imageURLs";
 
 const LargeScreenHeroImage = () => {
+  const [currentImg, setCurrentImg] = useState(largeScreenHeroImages[0]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTimeout(() => {
+        const randomIndex = Math.floor(
+          Math.random() * largeScreenHeroImages?.length
+        );
+        setCurrentImg(largeScreenHeroImages[randomIndex]);
+      }, 500);
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <Box
-      sx={{ display: { xs: "none", md: "block" }, objectFit: "cover" }}
+      sx={{
+        display: { xs: "none", md: "block" },
+        objectFit: "cover",
+      }}
+      alt="hero-image"
       component="img"
-      src="/assets/images/home/home5.jpg"
+      src={`/assets/images/home/${currentImg}`}
       height={1}
       flex={{ md: 2 }}
     />
